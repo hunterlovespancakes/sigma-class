02a18cdc5b372a92ad7e7504e3f4428e5966a5a6
hunterlovespancakes <hunterbest992@gmail.com>
2026-02-15 15:52:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,42 @@ const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
+const ADMIN_CODE = "SUPERADMIN123";
+let users = {};
+
 io.on("connection", (socket) => {
 
-    socket.on("join room", (room) => {
-        socket.join(room);
+    socket.on("join room", (data) => {
+        const { username, room } = data;
+
+        socket.username = username;
         socket.room = room;
+
+        socket.join(room);
+
+        users[socket.id] = username;
+
+        io.to(room).emit("user list", users);
+
+        if (room === ADMIN_CODE) {
+            socket.emit("admin");
+        }
     });
 
-    socket.on("chat message", (data) => {
-        io.to(socket.room).emit("chat message", data);
+    socket.on("chat message", (msg) => {
+        io.to(socket.room).emit("chat message", msg);
+    });
+
+    socket.on("kick user", (id) => {
+        if (socket.room === ADMIN_CODE) {
+            io.to(id).emit("kicked");
+            io.sockets.sockets.get(id)?.disconnect();
+        }
+    });
+
+    socket.on("disconnect", () => {
+        delete users[socket.id];
+        io.emit("user list", users);
     });
 
 });
