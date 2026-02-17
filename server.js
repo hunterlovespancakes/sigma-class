a98c5036a4860733585c0bcb68c2d970bd746659
hunterlovespancakes <hunterbest992@gmail.com>
2026-02-16 19:53:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,91 +1,108 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static("public"));
 
-const ADMIN_CODE = "huntercanrizzu"; // change this to your secret admin code
-
-let users = {}; // stores socket.id -> username
+let rooms = {}; 
+// Structure:
+// rooms = {
+//   roomName: {
+//     users: { socketId: username },
+//     admin: socketId
+//   }
+// }
 
 io.on("connection", (socket) => {
 
-    // When user joins a room
-    socket.on("join room", ({ username, room }) => {
-        socket.username = username;
-        socket.room = room;
+  socket.on("join room", ({ username, room }) => {
 
-        socket.join(room);
+    socket.join(room);
+    socket.username = username;
+    socket.room = room;
 
-        users[socket.id] = username;
+    if (!rooms[room]) {
+      rooms[room] = {
+        users: {},
+        admin: socket.id
+      };
+    }
 
-        // Send updated user list to that room only
-        io.to(room).emit("user list", getRoomUsers(room));
+    rooms[room].users[socket.id] = username;
 
-        // If they joined admin room, give admin powers
-        if (room === ADMIN_CODE) {
-            socket.emit("admin");
-        }
+    // Tell first person they are admin
+    if (rooms[room].admin === socket.id) {
+      socket.emit("admin");
+    }
+
+    // Send updated user list
+    io.to(room).emit("user list", rooms[room].users);
+  });
+
+
+  socket.on("chat message", (msg) => {
+
+    if (!socket.room) return;
+
+    const now = new Date();
+    const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+    io.to(socket.room).emit("chat message", {
+      name: socket.username,
+      message: msg,
+      time: time
     });
 
-    // When a message is sent
-    socket.on("chat message", (text) => {
-        if (!socket.room || !socket.username) return;
+  });
 
-        const messageData = {
-            user: socket.username,
-            text: text,
-            time: new Date().toLocaleTimeString([], {
-                hour: '2-digit',
-                minute: '2-digit'
-            })
-        };
 
-        io.to(socket.room).emit("chat message", messageData);
-    });
+  socket.on("kick user", (id) => {
 
-    // Admin kicking someone
-    socket.on("kick user", (id) => {
-        if (socket.room === ADMIN_CODE) {
-            const targetSocket = io.sockets.sockets.get(id);
-            if (targetSocket) {
-                targetSocket.emit("kicked");
-                targetSocket.disconnect();
-            }
-        }
-    });
+    if (!socket.room) return;
 
-    // When user disconnects
-    socket.on("disconnect", () => {
-        const room = socket.room;
-        delete users[socket.id];
+    const room = socket.room;
 
-        if (room) {
-            io.to(room).emit("user list", getRoomUsers(room));
-        }
-    });
+    if (rooms[room] && rooms[room].admin === socket.id) {
+      io.to(id).emit("kicked");
+      io.sockets.sockets.get(id)?.disconnect();
+    }
+
+  });
+
+
+  socket.on("disconnect", () => {
+
+    const room = socket.room;
+    if (!room || !rooms[room]) return;
+
+    delete rooms[room].users[socket.id];
+
+    // If admin leaves, make someone else admin
+    if (rooms[room].admin === socket.id) {
+      const users = Object.keys(rooms[room].users);
+      if (users.length > 0) {
+        rooms[room].admin = users[0];
+        io.to(users[0]).emit("admin");
+      }
+    }
+
+    io.to(room).emit("user list", rooms[room].users);
+
+    // If room empty, delete it
+    if (Object.keys(rooms[room].users).length === 0) {
+      delete rooms[room];
+    }
+
+  });
 
 });
 
-// Helper function to get only users in a room
-function getRoomUsers(room) {
-    const roomSockets = io.sockets.adapter.rooms.get(room);
-    let roomUsers = {};
-
-    if (roomSockets) {
-        roomSockets.forEach(id => {
-            if (users[id]) {
-                roomUsers[id] = users[id];
-            }
-        });
-    }
-
-    return roomUsers;
-}
 
 const PORT = process.env.PORT || 3000;
-
-http.listen(PORT, () => {
-    console.log("Sigma Class server running on port " + PORT);
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
